d457644a22f4cc439a7758c8a310a3f3b2a15948
infinite639 <tomthelizard1@gmail.com>
2026-08-16 01:35:57 +0400

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.set_page_config(page_title="RESILIA", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="RESILIA", layout="narrow", initial_sidebar_state="collapsed")
 
 st.markdown(
     """
